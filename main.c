f4622cd5a119b21a5f660175038b94c3c9189224
kevin <kin.siu1@ucdconnect.ie>
2026-03-23 17:28:38 +0000

    Created 2 new files, teamMember.c + teamMember.h: Intended to implement the teamMember tracking with the Nested linked list structure.
    Changing main for a better program flow offering 6 options, (following the Assignment 3 structure).
    
    To-do:
    1. implement add + remove member's
    2. modifying current gantt logic from list to LL implementation
    3. addTask Function
    4. printUserGantt() function:

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
+#include <stdlib.h>
 
 #include "gantt.h"
 #include "input.h"
+#include "teamMember.h"
 
 #define MAXARRAY 256
 
+void printMenu(void);
+
 int main(void) {
     printf("Welcome to the CTaskManager\n");
+
+    teamMember *memberHead = NULL; //init the teamMembers
 
     char userInput[MAXARRAY];
 
@@ -27,16 +33,58 @@ int main(void) {
             }
             break;
         }
-
         printf("Invalid Input!\n");
         }
 
+    int menuChoice = -1;
+    do {
+        printMenu();
+
+        if (fgets(userInput,MAXARRAY,stdin) == NULL) {
+            break;
+        }
+
+        removeNewline(userInput);
+
+        if (sscanf(userInput, "%d", &menuChoice) != 1) {
+            printf("Invalid input! Please enter a number.\n");
+            menuChoice = -1;
+            continue;
+        }
+
+        if (menuChoice == 6) break; // If user wants to exit, we don't have to go through all the other cases first
+
+        if (menuChoice < 1 || menuChoice > 6) {
+            printf("Please choose a number between 1 - 6\n");
+        }else {
+            switch (menuChoice) {
+                case 1: addMember(&memberHead); break;
+                case 2: removeMember(&memberHead); break;
+                case 3: addTask(memberHead); break;
+                case 4: printUserGantt(); break;
+                default: break:
+            }
+        }
+    }while (menuChoice != 6);
+
+    /*
     int taskNum = getNumOfTasks();
     Tasks userTaskArray[taskNum];
     //printf("%d\n", taskNum);
 
     getTaskInfo(userTaskArray,taskNum);
     printTask(userTaskArray,taskNum);
+    */
 
     return 0;
+}
+
+void printMenu(void) {
+    printf("=== Menu ===\n");
+    printf("1. Add a Team Member\n");
+    printf("2. Remove a Team Member\n");
+    printf("3. Add and Set a Task\n");
+    printf("4. Show Current Gantt chart\n");
+    printf("5. Show the Team Member and Tasks\n");
+    printf("6. Exit\n");
 }
